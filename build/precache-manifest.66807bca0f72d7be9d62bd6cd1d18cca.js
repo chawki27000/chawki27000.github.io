9ec7852bd4daf72a74daf492d166dab4426ed235
chawki benchehida <chawki27000@hotmail.com>
2019-07-13 11:48:12 +0100

    generate production website

---
+++ build/precache-manifest.66807bca0f72d7be9d62bd6cd1d18cca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3bff60ba7ce7d8b8570ffce9546023",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fef9ae72e0a797599cc0",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2d7c6ed91182fefec5db",
+    "url": "/static/js/2.475f8189.chunk.js"
+  },
+  {
+    "revision": "fef9ae72e0a797599cc0",
+    "url": "/static/js/main.7f32db99.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9087e63b15228e3dac8afc9885a09f80",
+    "url": "/static/media/profile.9087e63b.jpg"
+  }
+]);
